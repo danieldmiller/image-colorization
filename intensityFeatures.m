84ae2516521b0c77395b5ef33c1e0a23834dfa7d
Riza Hassan <ruhulruzbihan@gmail.com>
2020-10-20 23:10:11 -0500

    Update intensityFeatures.m

--- intensityFeatures.m
+++ intensityFeatures.m
@@ -7,7 +7,7 @@ imshow(A);
 
 [L,N] = superpixels(A,40);
 BW = boundarymask(L);
-% Get the average intensity values of all pixels within superpixel S
+% (1) Get the average intensity values of all pixels within superpixel S
 average_intensity1 = regionprops(L,A,'MeanIntensity');
 
 % Change struct into an array of double
@@ -15,9 +15,9 @@ average_intensity1 = struct2cell(average_intensity1);
 average_intensity1= cell2mat(average_intensity1);
 
 arraySize = size(average_intensity1,2);
-average_intensity2 = zeros(1,arraySize);
 
-% Get the neighboring average intensity values of the neighboring superpixels of Si
+% (2) Get the neighboring average intensity values of the neighboring superpixels of Si
+average_intensity2 = zeros(1,arraySize);
 for i =1:arraySize
     if(i==1)
         average_intensity2(1)=average_intensity1(i+1);
@@ -27,6 +27,16 @@ for i =1:arraySize
         average_intensity2(i)=1/2*(average_intensity1(i-1)+average_intensity1(i+1));
     end
 end
+
+% Store (1) and (2) in intensity_feature array of 2 rows 
+intensity_feature = zeros(2,arraySize);
+for i =1:arraySize
+%     First row refers to (1)
+    intensity_feature(1,i)=average_intensity1(i);
+%     Second row refers to (2)
+    intensity_feature(2,i)=average_intensity2(i);
+end
+
 
         
 

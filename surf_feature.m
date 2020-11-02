aa1609400d49603e6ec9cb1bb1aed7333e344644
Dementionless <56052390+Dementionless@users.noreply.github.com>
2020-11-01 22:17:37 -0500

    Fixed surf

--- surf_feature.m
+++ surf_feature.m
@@ -1,3 +1,14 @@
-function surf = surf_feature(I)
-surf = detectSURFFeatures(I);
+function [matched_source,matched_dest] = surf_feature(s,d)
+p1 = detectSURFFeatures(s).selectStrongest(128);
+p2 = detectSURFFeatures(d).selectStrongest(128);
+[fs,vs] = extractFeatures(s,p1);
+[fd,vd] = extractFeatures(d,p2);
+match_pairs = matchFeatures(fs,fd);
+matched_source = vs(match_pairs(:,1));
+matched_dest = vd(match_pairs(:,2));
+%%Display
+figure; 
+showMatchedFeatures(s,d,matched_source,matched_dest);
+legend('matched source','matched dest');
+
 end

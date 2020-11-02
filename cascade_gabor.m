2107c714cef79249f156cc453279697a488ce9bd
Dementionless <56052390+Dementionless@users.noreply.github.com>
2020-11-02 10:58:38 -0500

    I have to figure out how to make it accurate

---
+++ cascade_gabor.m
@@ -0,0 +1,16 @@
+function pixel = cascade_gabor(g_s, g_d, x,y)
+%CASCADE_GABOR Summary of this function goes here
+%   Detailed explanation goes here
+
+%Compute distance between g_s and g_d
+n = 40;
+pixel = 1;
+eps = 100;
+for i = 1:n
+    dist = sqrt((g_s(x,y,i) + g_d(x,y,i)).^2);
+    if(dist > eps)
+        pixel = 0;
+    end
+end
+end
+
